71bc587a8cea0fbf8e40548f23679ca437bc2e71
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-28 22:31:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+let l=[];
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+let ntask=document.getElementById('new-task-description');
+console.log(ntask.value);});
+//while (task!="")
+let b =document.getElementById('button');
+b.addEventListener("click", function(){
+let v=  document.getElementsByTagName('ul').innerHTml+=ntask;
+  console.log(v);
 });
